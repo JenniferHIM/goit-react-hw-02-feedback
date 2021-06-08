f717c9901313b51487274dfb1583b3c23cbacbe6
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-06-08 20:43:05 +0300

    added name of repository

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+
 import Statistics from './components/Statistics/Statistics';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Section from './components/Section/Section';
